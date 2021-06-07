84c92aa08df00bc515e3cd9f3d4ace095d5dda6b
TECRA <ayeshaaimen12@gmail.com>
2021-06-07 15:48:48 +0500

    6th commit

--- MyPractice/app/src/main/java/com/example/mypractice/MainActivity.java
+++ MyPractice/app/src/main/java/com/example/mypractice/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.linear_layout_2);
 
         textView=findViewById(R.id.textView3);
         button=findViewById(R.id.button3);
