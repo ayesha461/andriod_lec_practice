884441b22db7dd819819a87dc59b22774f449286
TECRA <ayeshaaimen12@gmail.com>
2021-06-07 15:38:01 +0500

    4th commit

--- MyPractice/app/src/main/java/com/example/mypractice/MainActivity.java
+++ MyPractice/app/src/main/java/com/example/mypractice/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.mypractice;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button button;
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.linear_layout);
+        setContentView(R.layout.activity_main);
+
+        textView=findViewById(R.id.textView3);
+        button=findViewById(R.id.button3);
+    }
+
+    public void buttonFun(View view) {
+        textView.setText("Some one is there");
     }
 }
