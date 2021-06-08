2679d32f7acde46d2f609e0416e72cf1cbf5e343
TECRA <ayeshaaimen12@gmail.com>
2021-06-08 14:28:57 +0500

    1st commit

--- MyPractice/app/src/main/java/com/example/mypractice/DetailActivity.java
+++ MyPractice/app/src/main/java/com/example/mypractice/DetailActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -13,7 +14,9 @@ public class DetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
+        TextView textView=findViewById(R.id.detailTtextVieww);
         Intent intent=getIntent();
         Log.d("checking",intent.getStringExtra("person name"));
+        textView.setText(intent.getStringExtra("person name"));
     }
 }
--- seekbar/app/src/main/java/com/example/seekbar/MainActivity.java
+++ seekbar/app/src/main/java/com/example/seekbar/MainActivity.java
@@ -3,12 +3,16 @@ package com.example.seekbar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
 
+    SeekBar seekBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        seekBar=findViewById(R.id.seekBar);
+        seekBar.setMax(40);
     }
 }
