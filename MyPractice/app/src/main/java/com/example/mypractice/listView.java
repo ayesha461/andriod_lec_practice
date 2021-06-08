1dddae5dba3759474ef2ff857a13a6dca8a02d12
TECRA <ayeshaaimen12@gmail.com>
2021-06-08 12:24:36 +0500

    addDataFromUserInlistView

--- MyPractice/app/src/main/java/com/example/mypractice/listView.java
+++ MyPractice/app/src/main/java/com/example/mypractice/listView.java
@@ -3,26 +3,45 @@ package com.example.mypractice;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 
 public class listView extends AppCompatActivity {
 
+    ListView listView;
+    ArrayList<String> friendListArray;
+    ArrayAdapter<String> arrayAdapter;
+    Button button;
+    EditText editText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.list_view);
-        ListView listView = findViewById(R.id.myListView);
+
+        /*list view binding*/
+        listView = findViewById(R.id.myListView);
         String[] friendArray = {"ayesha", "laby", "mutahira", "asma"};
-        ArrayList<String> friendListArray = new ArrayList<String>();
+        friendListArray = new ArrayList<String>();
         friendListArray.add("aimen");
         friendListArray.add("maria");
         friendListArray.add("tayyaba");
         friendListArray.add("usaid");
 
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, friendListArray);
+        arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, friendListArray);
+
+
+        /*list view binding end*/
+        button=findViewById(R.id.addBtn);
+        editText=findViewById(R.id.editItem);
+    }
+
+    public void addNewPerson(View view) {
+        friendListArray.add(editText.getText().toString());
         listView.setAdapter(arrayAdapter);
     }
 }
