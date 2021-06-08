6b2b7b56a9b587b16254816da82c13635c35372b
TECRA <ayeshaaimen12@gmail.com>
2021-06-08 15:53:12 +0500

    2nd commit

--- Animation/app/src/main/java/com/example/animation/MainActivity.java
+++ Animation/app/src/main/java/com/example/animation/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.animation;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void animation(View view) {
+        ImageView imageView=findViewById(R.id.imageView3);
+
+        /*for fading image*//*
+        imageView.animate().alpha(0).setDuration(1000);
+
+        *//*move on y-axis*//*
+
+        imageView.animate().translationXBy(500).setDuration(500);
+
+        *//*image roration*//*
+
+        imageView.animate().rotation(360).setDuration(1000);*/
+
+       /* fade+roration*//*
+        imageView.animate().rotation(720).alpha(0).setDuration(1000);*/
+
+       /* *//*scalling on image according to x and y axis*//*
+
+        imageView.animate().scaleX(0.5f);*/
+
+        /*scale x+y axis*/
+        imageView.animate().scaleX(0.5f).scaleY(0.5f);
     }
 }
